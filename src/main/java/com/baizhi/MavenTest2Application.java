ae04d19ee10f97a6e36f7734df8a64b7f6518bf9
fjq <931625521@qq.com>
2019-04-11 19:37:59 +0800

    v1.0

---
+++ src/main/java/com/baizhi/MavenTest2Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenTest2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/baizhi/entity/Person.java
@@ -0,0 +1,47 @@
+package com.baizhi.entity;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private Integer sex;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+}
